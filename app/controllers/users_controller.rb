b8610644a59ae5ecb9fd95694949035d40437bfc
あっつん <daisuke.samuser@gmail.com>
2016-09-28 09:28:39 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+  
   def show 
    @user = User.find(params[:id])
   end
@@ -7,6 +9,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
 def create
     @user = User.new(user_params)
@@ -23,5 +37,9 @@ end
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
