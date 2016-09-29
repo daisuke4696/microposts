d3877cb86112410e43bde158f2de913983ca6b46
あっつん <daisuke.samuser@gmail.com>
2016-09-29 07:38:30 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to @user
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
